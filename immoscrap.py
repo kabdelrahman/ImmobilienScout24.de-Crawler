f83d9a7c704f93d54779fca930ae6fce4e232a4e
Joshua Hruzik <Joshua.Hruzik@gmail.com>
2017-10-23 20:00:40 +0200

    Update immoscrap.py
    After Immobilienscout changed its website, this update will make the crawler work again. Some changes were made that enhance performance. Also a progress indicator was added.

--- immoscrap.py
+++ immoscrap.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+####PRELIMINARIES####
+
+#module import#
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
@@ -5,13 +10,24 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 import re
 
-site_list = ["https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete", "https://www.immobilienscout24.de/Suche/S-T/Haus-Miete",
-             "https://www.immobilienscout24.de/Suche/S-T/Wohnung-Kauf", "https://www.immobilienscout24.de/Suche/S-T/Haus-Kauf"]
-domain="https://www.immobilienscout24.de"
+
+#define sub-sites# 
+site_list = ["/Wohnung-Miete", 
+             "/Haus-Miete",
+             "/Wohnung-Kauf", 
+             "/Haus-Kauf"]
+
+#define top level domain
+domain="https://www.immobilienscout24.de/Suche/S-T"
+
+#initialize dataframe
 wohnung_data = DataFrame()
 
 
-def get_links(url):
+####EXTRACTING LINKS FOR CRAWLER####
+
+#get the last link for every asset type#
+def get_max(url):
     try:
         url = urlopen(url)
     except:
@@ -21,22 +37,19 @@ def get_links(url):
     except:
         print("Fehler beim Einlesen in BeautifulSoup")
     try:
-        link_list_raw = []
-        for option in site_extract.find_all("option"):
-            link_list_raw.append(option["value"])
+        max_link = max([int(n["value"]) for n in site_extract.find_all("option")])#get the maximum value for links in a specific sub-site
     except:
         print("Fehler beim Loop")
-    try:
-        link_list = [link for link in link_list_raw if "Suche" in link]
-    except:
-        print("Fehler beim Saeubern")
     else:
-        return link_list
+        return max_link
+    
 
+
+####EXTRACT DATA FROM EVERY SINGLE LINK####
     
 def get_data(url):
     try:
-        url_raw = url
+        url_raw = url#save url as string for real estate type
         url = urlopen(url)
     except HTTPError as e:
         return None
@@ -44,10 +57,10 @@ def get_data(url):
         return None
     try:
         site_extract = BeautifulSoup(url.read(), "lxml")
-        rawdata_extract = site_extract.find_all("div", {"class":"result-list-entry__data"})
+        rawdata_extract = site_extract.find_all("div", {"class":"result-list-entry__data"})#extract every result box
     except AttributeError as e:
         return None
-    global wohnung_data
+    global wohnung_data#use global dataframe
     price = []
     size = []
     location = []
@@ -55,15 +68,15 @@ def get_data(url):
     immo_type = []
     for i in range(1,len(rawdata_extract)):
         try:
-            price.append(rawdata_extract[i].find_all("dd")[0].get_text().strip())
+            price.append(rawdata_extract[i].find_all("dd")[0].get_text().strip())#extract price
         except:
             price.append(None)
         try:
-            size.append(rawdata_extract[i].find_all("dd")[1].get_text().strip())
+            size.append(rawdata_extract[i].find_all("dd")[1].get_text().strip())#extract size
         except:
             size.append(None)
         try:
-            location.append(rawdata_extract[i].find_all("div", {"class":"result-list-entry__address nine-tenths"})[0].get_text().strip())
+            location.append(rawdata_extract[i].find_all("div", {"class":"result-list-entry__address"})[0].get_text().strip())#extract location
         except:
             location.append(None)
         if "/Wohnung" in url_raw:
@@ -74,22 +87,44 @@ def get_data(url):
             ownership.append("Miete")
         elif "-Kauf" in url_raw:
             ownership.append("Kauf")
-    wohnung_data = wohnung_data.append(DataFrame({"price":price, "size":size,"location":location, "real_estate":immo_type, "ownership":ownership}), ignore_index=True)
+    wohnung_data = wohnung_data.append(DataFrame({"price":price, 
+                                                  "size":size,
+                                                  "location":location, 
+                                                  "real_estate":immo_type, 
+                                                  "ownership":ownership}), 
+    ignore_index=True)
     
 
 
+
+####START CRAWLER####
+
 def immo_crawl(site_list):
-    link_list = []
+    max_dict = {}#initialize dictionary for maximum values of links
     for site in site_list:
-        link_list = link_list + get_links(site)
-    for link in link_list:
-        print("Bearbeiten des Links: "+link)
-        get_data(domain+link)
+        max_dict[site] = get_max(domain+site)#associate maximal link value with specific sub-site
+    link_list_full = []#initialize list for full links to crawl#
+    for site in max_dict:
+        for i in range(1,max_dict[site]):
+            link_list_full.append(domain+"/P-"+str(i)+site)#populate link_list_full
+    link_count = 1#start for progress indicator
+    len_link_list_full = len(link_list_full)#end for progress indicator
+    for link in link_list_full:
+        print("Crawling: "+link+" (link #"+str(link_count)+" of "+str(len_link_list_full)+")")#print progress
+        link_count += 1#add to progress indicator
+        get_data(link)
+    
+    
         
-immo_crawl(site_list)
+immo_crawl(site_list)#start crawler
 
-wohnung_data.to_csv("~/wohnung_data_raw.csv", sep=";", index=False)
 
+####PROCESS DATA####
+
+wohnung_data.to_csv("wohnung_data_raw.csv", sep=";", index=False)#export unprocessed data
+
+
+#clean data#
 def clean_pricesize(data):
     data = data.replace("€", "")
     data = data.replace(".", "")
@@ -97,6 +132,7 @@ def clean_pricesize(data):
     data = re.sub(re.compile(" \D.*"), "", data)
     data = data.strip()
     return data
+
 
 def get_firstlayer(data):
     fist_layer = data.split(",")[0]
@@ -112,4 +148,4 @@ wohnung_data_clean["size"] = wohnung_data_clean["size"].apply(clean_pricesize)
 wohnung_data_clean["location_first"] = wohnung_data_clean["location"].apply(get_firstlayer)
 wohnung_data_clean["location_last"] = wohnung_data_clean["location"].apply(get_lastlayer)
 
-wohnung_data_clean.to_csv("~/wohnung_data_clean.csv", sep=";", index=False)
+wohnung_data_clean.to_csv("wohnung_data_clean.csv", sep=";", index=False)
